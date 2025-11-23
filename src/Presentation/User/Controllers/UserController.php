5e85faf1e2424d74f4816821653383e394c3977d
Dev <denis.03.ermakov@gmail.com>
2025-11-23 15:01:29 +0500

    feature: added user controller with method me. tests is green.

---
+++ src/Presentation/User/Controllers/UserController.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Presentation\User\Controllers;
+
+use Application\User\Services\Contracts\UserServiceContract;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
+use Presentation\BaseController;
+use OpenApi\Attributes as OA;
+use Presentation\User\Resources\UserResource;
+
+/**
+ * Controller for users
+ */
+class UserController extends BaseController
+{
+    public function __construct(
+        protected readonly UserServiceContract $userService,
+    ) {}
+
+    public function me(Request $request): JsonResponse
+    {
+        $user = $this->userService->me();
+
+        return Response::json(new UserResource($user));
+    }
+}
