5e490dbe4f1085beb70a4c2bdfb524fbe7d60472
Dev <denis.03.ermakov@gmail.com>
2025-11-23 13:12:32 +0500

    tests: added tests for route api.auth.login. tests is red.

---
+++ tests/Feature/Presentation/User/Controllers/AuthControllerTest.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Presentation\User\Controllers;
+
+use Application\User\DTO\UserDTO;
+use Domain\User\Factories\UserFactory;
+use Domain\User\Models\User;
+use Tests\TestCase;
+
+class AuthControllerTest extends TestCase
+{
+    public function test_login_success(): void
+    {
+        // Data for testing
+        $userData = UserDTO::from((new UserFactory())->definition());
+        $userData->password = $this->userPassword;
+
+        // User for testing
+        User::create($userData->toArray());
+
+        // Send API Request
+        $response = $this->post(route('api.auth.login'), $userData->only('nickname', 'password')->toArray());
+
+        // Check asserts
+        $response->assertOk();
+        $response->assertJsonStructure([
+            'access_token',
+            'token_type',
+            'expires_in',
+        ]);
+    }
+
+    public function test_login_bad_nickname(): void
+    {
+        // Data for testing
+        $userData = UserDTO::from((new UserFactory())->definition());
+        $userData->password = $this->userPassword;
+
+        // User for testing
+        User::create($userData->toArray());
+
+        // Data for request
+        $data = [
+            'nickname' => fake()->userName(), // false value
+            'password' => $userData->password, // true value
+        ];
+
+        // Send API Request
+        $response = $this->post(route('api.auth.login'), $data);
+
+        // Check asserts
+        $response->assertBadRequest();
+        $response->assertClientError();
+        $response->assertJson(['message' => 'Nickname or password incorrect.']);
+    }
+
+    public function test_login_bad_password(): void
+    {
+        // Data for testing
+        $userData = UserDTO::from((new UserFactory())->definition());
+        $userData->password = $this->userPassword;
+
+        // User for testing
+        User::create($userData->toArray());
+
+        // Data for request
+        $data = [
+            'nickname' => $userData->nickname, // true value
+            'password' => fake()->password(), // false value
+        ];
+
+        // Send API Request
+        $response = $this->post(route('api.auth.login'), $data);
+
+        // Check asserts
+        $response->assertBadRequest();
+        $response->assertClientError();
+        $response->assertJson(['message' => 'Nickname or password incorrect.']);
+    }
+}
