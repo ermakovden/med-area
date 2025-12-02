1e76574b925b0aa50856ba7915d8dca7aecde536
Dev <denis.03.ermakov@gmail.com>
2025-12-02 15:36:36 +0500

    tests: added tests for route api.users.analysis.index. tests is red.

--- tests/Feature/Presentation/Analys/Controllers/UserAnalysControllerTest.php
+++ tests/Feature/Presentation/Analys/Controllers/UserAnalysControllerTest.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Tests\Feature\Presentation\Analys\Controllers;
 
+use Application\Analys\DTO\Filters\FilterUserAnalysDTO;
 use Application\Analys\DTO\Requests\CreateUserAnalysisRequestDTO;
 use Application\Analys\DTO\UserAnalysDTO;
+use Domain\Analys\Enums\Analys;
 use Domain\Analys\Factories\UserAnalysFactory;
 use Domain\Analys\Models\UserAnalys;
 use Tests\TestCase;
@@ -169,5 +171,82 @@ class UserAnalysControllerTest extends TestCase
         foreach ($dto->analysis as $analys) {
             $this->assertDatabaseMissing(UserAnalys::class, $analys->toArray());
         }
+    }
+
+    public function test_user_analysis_index_success(): void
+    {
+        // Auth user for testing
+        $user = $this->authUser();
+
+        // Data for testing
+        $count = 3;
+        $factory = new UserAnalysFactory();
+
+        // Data with another values for testing filters
+        UserAnalys::factory(10)
+            ->for($user)
+            ->createMany();
+
+        // Delete analys_id for testing filters
+        UserAnalys::query()->whereAnalysId(Analys::D3->value)->delete();
+
+        // Data UserAnalys Models for searching with filters (user_id, analys_id)
+        for ($i = 0; $i < $count; $i++) {
+            UserAnalys::query()->create(array_merge($factory->definition(), [
+                'user_id' => $user->id,
+                'analys_id' => Analys::D3,
+            ]));
+        }
+
+        // Filters for testing
+        $filters = FilterUserAnalysDTO::from([
+            'user_ids' => [$user->id],
+            'analys_ids' => [Analys::D3],
+        ]);
+
+        // Send API Request with filters
+        $response = $this->get(route('api.users.analysis.index', array_merge([
+            'userId' => $user->id,
+        ], $filters->toArray())));
+
+        // Check asserts
+        $response->assertOk();
+        $response->assertJsonCount($count);
+    }
+
+    public function test_user_analysis_index_forbidden_unreal_user_id(): void
+    {
+        // Auth user for testing
+        $user = $this->authUser();
+        $user2 = $this->getUser();
+
+        // Filters for testing
+        $filters = FilterUserAnalysDTO::from(['user_ids' => [$user->id]]);
+
+        // Send API Request with filters and another user_id in url
+        $response = $this->get(route('api.users.analysis.index', array_merge([
+            'userId' => $user2->id,
+        ], $filters->toArray())));
+
+        // Check assert forbidden, 403 http code
+        $response->assertForbidden();
+    }
+
+    public function test_user_analysis_index_forbidden_filter_user_ids(): void
+    {
+        // Auth user for testing
+        $user = $this->authUser();
+        $user2 = $this->getUser();
+
+        // Filters with another user_id for testing
+        $filters = FilterUserAnalysDTO::from(['user_ids' => [$user->id, $user2->id]]);
+
+        // Send API Request with bad filters
+        $response = $this->get(route('api.users.analysis.index', array_merge([
+            'userId' => $user->id,
+        ], $filters->toArray())));
+
+        // Check assert forbidden, 403 http code
+        $response->assertForbidden();
     }
 }
