1ce2f0e883721bb2e2e5e5d866614c23ce9f382b
Dev <denis.03.ermakov@gmail.com>
2025-12-14 16:30:34 +0500

    feature: added route api.files.index, file controller - index method. tests is green.

--- src/Presentation/File/Controllers/FileController.php
+++ src/Presentation/File/Controllers/FileController.php
@@ -8,6 +8,7 @@ use Application\S3\Services\Contracts\S3ServiceContract;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Response;
 use Presentation\BaseController;
+use Presentation\File\Requests\IndexFileRequest;
 use Presentation\File\Requests\UploadFilesRequest;
 use Presentation\File\Resources\FileResourceCollection;
 use Shared\Enums\Storage as EnumsStorage;
@@ -33,5 +34,14 @@ class FileController extends BaseController
         }
 
         return Response::json(FileResourceCollection::make($models), 201);
+    }
+
+    public function index(IndexFileRequest $request): JsonResponse
+    {
+        $filters = $request->getDTO();
+
+        $files = $this->s3Service->getFiles($filters);
+
+        return Response::json(new FileResourceCollection($files), 200);
     }
 }
--- src/Presentation/File/routes/api.files.php
+++ src/Presentation/File/routes/api.files.php
@@ -6,4 +6,5 @@ use Presentation\File\Controllers\FileController;
 
 Route::prefix('files')->middleware(['auth'])->group(function () {
     Route::post('', [FileController::class, 'upload'])->name('api.files.upload');
+    Route::get('', [FileController::class, 'index'])->name('api.files.index');
 });
