564e19439f128fd73d841bdd5f4def2877537535
Dev <denis.03.ermakov@gmail.com>
2025-12-19 12:55:50 +0500

    feature: added migration, model, dto for recognise request. added recognise status for that model

---
+++ database/migrations/2025_12_19_072740_create_recognise_requests_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recognise_requests', function (Blueprint $table) {
+            $table->id()->primary();
+            $table->foreignUuid('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignUuid('file_id')->constrained()->cascadeOnDelete();
+            $table->string('operation_id');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recognise_requests');
+    }
+};
---
+++ src/Domain/AI/Recognise/Models/RecogniseRequest.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Domain\AI\Recognise\Models;
+
+use Domain\AI\Recognise\Enums\RecogniseStatus;
+use Domain\File\Models\File;
+use Domain\User\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * @property int $id
+ * @property string $user_id
+ * @property string $file_id
+ * @property string $operation_id
+ * @property RecogniseStatus $status
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+ * @property-read int|null $notifications_count
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|File newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|File newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|File query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|File whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|File whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|File whereFileId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|File whereOperationId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|File whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|File whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|File whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class RecogniseRequest extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'user_id',
+        'file_id',
+        'operation_id',
+        'status',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'id' => 'int',
+            'status' => RecogniseStatus::class,
+        ];
+    }
+
+    /**
+     * @return BelongsTo<User, $this>
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * @return BelongsTo<File, $this>
+     */
+    public function file(): BelongsTo
+    {
+        return $this->belongsTo(File::class);
+    }
+}
