a385ea4cbf3d8c7cd29e909b892efc5a4c016914
Dev <denis.03.ermakov@gmail.com>
2025-12-25 17:38:00 +0500

    feature: added method recognise async for y vision ocr service. tests is green.

--- src/Application/AI/Recogniser/Services/Contracts/RecogniserServiceContract.php
+++ src/Application/AI/Recogniser/Services/Contracts/RecogniserServiceContract.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Application\AI\Recogniser\Services\Contracts;
 
+use Application\AI\Recogniser\DTO\Requests\RecogniseAsyncRequestDTO;
+use Application\AI\Recogniser\DTO\Responses\RecogniseAsyncResponse;
+
 interface RecogniserServiceContract
 {
+    public function recogniseAsync(RecogniseAsyncRequestDTO $request): RecogniseAsyncResponse;
 }
--- src/Application/AI/Recogniser/Services/YVisionOCRService.php
+++ src/Application/AI/Recogniser/Services/YVisionOCRService.php
@@ -4,11 +4,67 @@ declare(strict_types=1);
 
 namespace Application\AI\Recogniser\Services;
 
+use Application\AI\Recogniser\DTO\RecogniseURLParamsDTO;
+use Application\AI\Recogniser\DTO\Requests\RecogniseAsyncRequestDTO;
+use Application\AI\Recogniser\DTO\Responses\RecogniseAsyncResponse;
 use Application\AI\Recogniser\Services\Contracts\RecogniserServiceContract;
+use Illuminate\Support\Facades\Http;
+use Shared\Enums\AuthTokenType;
+use Shared\Exceptions\ServerErrorException;
+use Shared\Services\BaseExternalService;
 
 /**
- * Docs see here: https://yandex.cloud/ru/docs/vision/
+ * Yandex Vision OCR Service
+ *
+ * See: https://yandex.cloud/ru/docs/vision/
  */
-class YVisionOCRService implements RecogniserServiceContract
+class YVisionOCRService extends BaseExternalService implements RecogniserServiceContract
 {
+    public function __construct()
+    {
+        parent::__construct();
+
+        $authToken = config('yc.ocr.secret');
+        $this->setAuthorization($authToken, AuthTokenType::API_KEY);
+
+        $this->setURLParams(RecogniseURLParamsDTO::from([
+            'endpoint' => config('yc.ocr.endpoint'),
+            'version' => config('yc.ocr.version'),
+            'module' => 'ocr',
+        ]));
+    }
+
+    public function recogniseAsync(RecogniseAsyncRequestDTO $request): RecogniseAsyncResponse
+    {
+        $this->setURLResourceParam('recognizeTextAsync');
+
+        try {
+            $response = Http::withHeaders($this->getBaseHeaders())
+                ->withUrlParameters($this->urlParams->toArray())
+                ->post(
+                    $this->getURITemplate(),
+                    $request->toArray(),
+                );
+
+            if ($response->failed()) {
+                $response->throw();
+            }
+
+            \Log::info($response->json());
+            return RecogniseAsyncResponse::from($response->json());
+
+        } catch (\Exception $e) {
+            \Log::error('Error when try start recognise async', [
+                'class' => YVisionOCRService::class,
+                'method' => 'recogniseAsync',
+                'message' => $e->getMessage(),
+            ]);
+            throw new ServerErrorException();
+        }
+    }
+
+    public function getURITemplate(): string
+    {
+        return '{+endpoint}/{module}/{version}/{resource}';
+    }
 }
