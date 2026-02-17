a13c72202cf70154b4c729fb92ffc1cddc828b9c
Dev <denis.03.ermakov@gmail.com>
2026-02-17 15:45:50 +0500

    feature: added route api.analysis.recogniser.create with documentation. tests is green.

--- src/Presentation/Analys/routes/api.analysis.php
+++ src/Presentation/Analys/routes/api.analysis.php
@@ -3,7 +3,13 @@
 declare(strict_types=1);
 
 use Presentation\Analys\Controllers\AnalysController;
+use Presentation\Analys\Controllers\RecogniserController;
 
 Route::prefix('analysis')->group(function () {
     Route::get('', [AnalysController::class, 'index'])->name('api.analysis.index');
+
+    // Recogniser
+    Route::prefix('recogniser')->group(function () {
+        Route::post('', [RecogniserController::class, 'store'])->name('api.analysis.recogniser.create');
+    });
 });
